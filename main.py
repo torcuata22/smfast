8821158c93d3056f1f8418f0b30417df6bab47f1
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-21 16:28:14 -0400

    post

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Welcome to my FastAPI"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "this is your post"}
+
+@app.post("/createposts")
+def createposts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"message":"post has been created!"}
+    
