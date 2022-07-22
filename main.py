d5d09686664dc3d07eefe4c8de69d6ec591a6f93
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-22 15:16:56 -0400

    error handling

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from ast import Str
 from typing import Optional #to make a field optional in our class
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from fastapi.params import Body
 from pydantic import BaseModel  
+from random import randrange
 
 app = FastAPI()
 
@@ -12,7 +13,12 @@ class Post(BaseModel):
     published: bool=True #this is an optional field and I'm giving it a default value
     rating:Optional[int]=None #this is fully optional and, if not provided, will default to null 
 
-my_posts=[{'title': 'title of post1', 'content': 'content of post1', 'id':'1'},{'title': 'favorite foods', 'content': 'I like pasta', 'id':'2'}] 
+my_posts=[{'title': 'title of post1', 'content': 'content of post1', 'id':1},{'title': 'favorite foods', 'content': 'I like pasta', 'id':2}] 
+
+def find_post(id):
+    for p in my_posts:
+        if p['id']==id:
+            return p
 
 @app.get("/")
 def root():
@@ -20,11 +26,19 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "this is your post"}
+    return {"data": my_posts}
 
 @app.post("/posts")
-def createposts(post:Post):
-    print(post.rating) 
-    print(post.dict())
-    return {"data":post} #I can do this because pydantic allows me to convert ot dictionary
-    
+def create_posts(post:Post):
+    post_dict=post.dict() #pydantic allows me to convert ot dictionary
+    post_dict['id']=randrange(0,100000)
+    my_posts.append(post_dict)
+    return {"data":post_dict} 
+
+@app.get("/posts/{id}")
+def get_post(id:int, response: Response):
+    post=find_post(id)
+    if not post:
+        response.status_code= status.HTTP_404_NOT_FOUND
+        return {'message':f"post with id:{id} was not found"}
+    return{"post_detail":post}
