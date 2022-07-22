8b57eeda287996bc8f79cc7b08adec99523a37cb
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-22 14:30:49 -0400

    variable for posts for now

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ class Post(BaseModel):
     published: bool=True #this is an optional field and I'm giving it a default value
     rating:Optional[int]=None #this is fully optional and, if not provided, will default to null 
 
+my_posts=[{'title': 'title of post1', 'content': 'content of post1', 'id':'1'},{'title': 'favorite foods', 'content': 'I like pasta', 'id':'2'}] 
+
 @app.get("/")
 def root():
     return {"message": "Welcome to my FastAPI"}
@@ -20,8 +22,9 @@ def root():
 def get_posts():
     return {"data": "this is your post"}
 
-@app.post("/createposts")
-def createposts(new_post:Post):
-    print(new_post.rating)
-    return {"data":"new post"}
+@app.post("/posts")
+def createposts(post:Post):
+    print(post.rating) 
+    print(post.dict())
+    return {"data":post} #I can do this because pydantic allows me to convert ot dictionary
     
