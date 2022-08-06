d0382d006a63f49878177962e56277b79150a767
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-08-06 15:29:24 -0400

    added SQL queries

--- app/main.py
+++ app/main.py
@@ -4,6 +4,11 @@ from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel  
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
+
 
 app = FastAPI()
 
@@ -11,9 +16,18 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool=True #this is an optional field and I'm giving it a default value
-    rating:Optional[int]=None #this is fully optional and, if not provided, will default to null 
+    #rating:Optional[int]=None #this is fully optional and, if not provided, will default to null 
 
-
+while True: 
+    try:
+        conn=psycopg2.connect(host='localhost', database='smfast', user='postgres', password='admin9325505', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successful")
+        break
+    except Exception as error:
+        print("connection to database failed")
+        print("Error:", error)
+        time.sleep(2)
 
 my_posts=[{'title': 'title of post1', 'content': 'content of post1', 'id':1},{'title': 'favorite foods', 'content': 'I like pasta', 'id':2}] 
 
@@ -27,46 +41,65 @@ def find_index_post(id):
         if p['id'] == id:
             return i
 
+
 @app.get("/")
 def root():
     return {"message": "Welcome to my FastAPI"}
 
+
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts """) 
+    posts =cursor.fetchall() 
+    print(posts)
+    return {"data": posts}
+
 
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_posts(post:Post):
-    post_dict=post.dict() #pydantic allows me to convert ot dictionary
-    post_dict['id']=randrange(0,100000)
-    my_posts.append(post_dict)
-    return {"data":post_dict} 
+    #this is the code to work from database:
+    cursor.execute("""INSERT INTO posts (title, content, published)VALUES(%s,%s, %s) RETURNING * """, 
+                   (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit() #commits to DB
+    return {"data":new_post}
+    #these lines were for working with hard coded data
+    # post_dict=post.dict() #pydantic allows me to convert ot dictionary
+    # post_dict['id']=randrange(0,100000)
+    # my_posts.append(post_dict)
+      
 
 @app.get("/posts/{id}")
 def get_post(id:int, response: Response):
-    post=find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """,(str(id),))
+    post=cursor.fetchone()
     if not post:
         # response.status_code= status.HTTP_404_NOT_FOUND
         # return {'message':f"post with id:{id} was not found"} Replaced by:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} was not found")
     return{"post_detail":post}
 
+
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id:int):
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute("""DELETE FROM posts WHERE id = %s returning *""", (str(id),))
+    deleted_post=cursor.fetchone()
+    conn.commit()
+    
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} doesn't exist")
-    my_posts.pop(index)
+    
     return Response(status_code=status.HTTP_204_NO_CONTENT) #to make sure we are not sending data back and get an error
+
 
 @app.put('/posts/{id}')
 def update_post(id:int, post:Post):
-    index = find_index_post(id) #looks for index in array
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id=%s RETURNING *""", (post.title, post.content, post.published, str(id)) )
+    updated_post=cursor.fetchone()
+    conn.commit()
+    
    
-    if index == None:
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} doesn't exist")
     
-    post_dict=post.dict() #convert front end data into python dict
-    post_dict['id']=id
-    my_posts[index] = post_dict
-    return {'data':post_dict}
+    return {'data':updated_post}
